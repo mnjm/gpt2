8a64940b0e2fd9b96d6b3360c83fb5ec9f117bb3
Manjunath Mohan <manjunat.mohan@gmail.com>
2025-07-13 19:47:46 +0530

    weight init with residual init scheme

--- train_gpt2.py
+++ train_gpt2.py
@@ -10,18 +10,21 @@ from utils import get_torch_device, GPT2DataLoaderLite
 device = get_torch_device()
 print(f"using device: {device}")
 
-torch.manual_seed(31415)
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+    
 
 @dataclass
 class GPTConfig:
-    block_size: int = 1024 # max sequence length
-    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|>
-    n_layer: int = 12 # number of layers
-    n_head: int = 12 # number of heads
-    n_embd: int = 768 # embedding dims
+    block_size: int = 1024  # max sequence length
+    vocab_size: int = 50257  # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|>
+    n_layer: int = 12  # number of layers
+    n_head: int = 12  # number of heads
+    n_embd: int = 768  # embedding dims
+    
 
 class CausalSelfAttention(nn.Module):
-    
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
@@ -29,125 +32,179 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.IS_RESIDUAL_PROJ_LYR = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         # below is not a "bias", it is a mask / trill but following OpenAI/HF naming so..
-        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
-    
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
     def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embd dim (n_embd)
+        B, T, C = x.size()  # batch size, sequence length, embd dim (n_embd)
         # calculate query, key, values for all heads in batch and move forward to the batch
         # nh is no. of heads, hs is head size, and C is number of channles = nh * hs
         q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(
+            1, 2
+        )  # (B, nh, T, hs)
         # attention (materializes the large (T, T) matrix for all the queries and keys)
         attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-        attn = attn.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        attn = attn.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
         attn = F.softmax(attn, dim=-1)
-        y = attn @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        y = attn @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = (
+            y.transpose(1, 2).contiguous().view(B, T, C)
+        )  # re-assemble all head outputs side by side
         # output projection
         y = self.c_proj(y)
         return y
+    
 
 class MLP(nn.Module):
-    
     def __init__(self, config):
         super().__init__()
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
-    
+        self.c_proj.IS_RESIDUAL_PROJ_LYR = 1
+
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
         return x
 
+
 class Block(nn.Module):
-    
     def __init__(self, config):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config.n_embd)
         self.attn = CausalSelfAttention(config)
         self.ln_2 = nn.LayerNorm(config.n_embd)
         self.mlp = MLP(config)
-    
+
     def forward(self, x):
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
 
+
 class GPT(nn.Module):
-    
     def __init__(self, config):
         super().__init__()
         self.config = config
-        
-        self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
-            h = nn.ModuleList(Block(config) for _ in range(config.n_layer)),
-            ln_f = nn.LayerNorm(config.n_embd),
-        ))
+
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embd),
+                wpe=nn.Embedding(config.block_size, config.n_embd),
+                h=nn.ModuleList(Block(config) for _ in range(config.n_layer)),
+                ln_f=nn.LayerNorm(config.n_embd),
+            )
+        )
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
-        
+
         # weight sharing scheme
         self.transformer.wte.weight = self.lm_head.weight
-    
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            # Residual steam connections (here residual projection layer c_proj in attn and mlp of every block)
+            # should be inited with scale of (1/sqrt(N)) where N is the number of residual connections, so that
+            # overall scale/std is 1.
+            if hasattr(module, "IS_RESIDUAL_PROJ_LYR"):
+                # 2 * n_layers because every layer (block) has 2 residual connections
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
     def forward(self, idx, target=None):
         B, T = idx.size()
-        assert T <= self.config.block_size, f"Cannt forward sequence of length {T}, block size is only {self.config.block_size}"
-        pos_emb = self.transformer.wpe(torch.arange(0, T, dtype=torch.long, device=idx.device)) # pos embedding (T, n_embd)
-        tok_emb = self.transformer.wte(idx) #  token embedding (B, T, n_embd)
-        x = tok_emb + pos_emb # (B, T, n_embd)
+        assert T <= self.config.block_size, (
+            f"Cannt forward sequence of length {T}, block size is only {self.config.block_size}"
+        )
+        pos_emb = self.transformer.wpe(
+            torch.arange(0, T, dtype=torch.long, device=idx.device)
+        )  # pos embedding (T, n_embd)
+        tok_emb = self.transformer.wte(idx)  #  token embedding (B, T, n_embd)
+        x = tok_emb + pos_emb  # (B, T, n_embd)
         for block in self.transformer.h:
             x = block(x)
-        x = self.transformer.ln_f(x) # layer norm
-        logits = self.lm_head(x) # (B, T, vocab_size)
+        x = self.transformer.ln_f(x)  # layer norm
+        logits = self.lm_head(x)  # (B, T, vocab_size)
         loss = None
         if target is not None:
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), target.view(-1), ignore_index=-1)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)), target.view(-1), ignore_index=-1
+            )
         return logits, loss
-    
+
     @classmethod
     def from_pretrained(cls, model_type):
-        """ Loads pretrained GPT-2 model weights from huggingface """
-        assert model_type in ('gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl')
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in ("gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl")
         from transformers import GPT2LMHeadModel
+
         print(f"Loading weights from pretrained gpt: {model_type}")
-        
+
         # n_layer, n_head and n_embd are determined from model_type
         config_args = {
-            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params 
-            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
         }[model_type]
-        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
-        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        config_args["vocab_size"] = 50257  # always 50257 for GPT model checkpoints
+        config_args["block_size"] = 1024  # always 1024 for GPT model checkpoints
         # create a from-scratch init minGPT model
         config = GPTConfig(**config_args)
         model = GPT(config)
         sd = model.state_dict()
         sd_keys = sd.keys()
-        sd_keys = [ k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer
-        
+        sd_keys = [
+            k for k in sd_keys if not k.endswith(".attn.bias")
+        ]  # discard this mask / buffer
+
         # init a hf/transformer model
         model_hf = GPT2LMHeadModel.from_pretrained(model_type)
         sd_hf = model_hf.state_dict()
-        
+
         # copy while ensuring all of the params are aligned and match in names and shapes
         sd_keys_hf = sd_hf.keys()
-        sd_keys_hf = [ k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these masks
-        sd_keys_hf = [ k for k in sd_keys_hf if not k.endswith('.attn.bias')] # ignore biases, same as masks
-        transposed = [ 'attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight' ]
+        sd_keys_hf = [
+            k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")
+        ]  # ignore these masks
+        sd_keys_hf = [
+            k for k in sd_keys_hf if not k.endswith(".attn.bias")
+        ]  # ignore biases, same as masks
+        transposed = [
+            "attn.c_attn.weight",
+            "attn.c_proj.weight",
+            "mlp.c_fc.weight",
+            "mlp.c_proj.weight",
+        ]
         # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
         # this means that we have to transpose these weights when we import them
-        assert len(sd_keys_hf) == len(sd_keys), (f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}")
+        assert len(sd_keys_hf) == len(sd_keys), (
+            f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        )
         for k in sd_keys_hf:
             if any(k.endswith(w) for w in transposed):
                 # special treatment for the Conv1D weights we need to transpose
@@ -159,17 +216,17 @@ class GPT(nn.Module):
                 assert sd_hf[k].shape == sd[k].shape
                 with torch.no_grad():
                     sd[k].copy_(sd_hf[k])
-        
+
         return model
 
+
 if __name__ == "__main__":
-    
     model = GPT(GPTConfig())
     model.to(device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-    
+
     train_loader = GPT2DataLoaderLite("input.txt", B=4, T=32)
-    
+
     # training
     for i in range(50):
         x, y = train_loader.next_batch()
